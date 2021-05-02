778aaad6639151fd41d346d88f713c73fa654f67
Israel Akpan <isrealakpan296@gmail.com>
2021-05-02 04:47:07 +0100

    added app colors

---
+++ lib/cores/constants/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// This is the default Card Color
+Color cardColor = Color(0xFFFFFFFF);
+
+/// This is the default title text Color
+Color titleColor = Color(0xFF0F0627);
+
+/// This is the default button Color
+Color buttonColor = Color(0xFF0047FF);
+
+/// This is the default subtitle text Color
+Color subtitleColor = Color(0xFF9A96A4);
+
+/// This is the default background Color1
+Color backgroundColor1 = Color(0xFF3F74FF);
+
+/// This is the default background Color1
+Color backgroundColor2 = Color(0xFF0047FF);
