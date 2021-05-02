436a8b7f636c78887798fe56befe95941ffaef84
Israel Akpan <isrealakpan296@gmail.com>
2021-05-02 07:07:46 +0100

    defined text style

---
+++ lib/cores/constants/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+/// Defining global style for text
+TextStyle textStyle = TextStyle(
+  fontWeight: FontWeight.w600,
+  fontSize: 32.0,
+  fontStyle: FontStyle.normal,
+  color: cardColor,
+);
