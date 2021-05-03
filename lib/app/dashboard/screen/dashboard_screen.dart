a60447a879d560dd135ff3e856dda21a859de5a9
Israel Akpan <isrealakpan296@gmail.com>
2021-05-03 05:10:00 +0100

    [WIP] - app dashboard

--- lib/app/dashboard/screen/dashboard_screen.dart
+++ lib/app/dashboard/screen/dashboard_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import '../../../cores/constants/colors.dart';
-import '../../../cores/constants/styles.dart';
+import '../widgets/xafe_animated_index.dart';
 import '../widgets/xafe_bottom_navigation_bar.dart';
+import 'categories/categories_screen.dart';
+import 'home/home_screen.dart';
 
 /// [Dashboard] screen or app main home page
 class DashboardScreen extends StatelessWidget {
@@ -12,57 +14,11 @@ class DashboardScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: backgroundColor1,
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.4,
-            ),
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: cardColor,
-                  ),
-                  child: ListView.separated(
-                    itemBuilder: (context, i) {
-                      return Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                '📄 Bill Payments',
-                                style: textStyle.copyWith(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.w500,
-                                  color: titleColor,
-                                ),
-                              ),
-                            ),
-                            Text(
-                              '\$500.00',
-                              style: textStyle.copyWith(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w500,
-                                color: titleColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    separatorBuilder: (context, i) {
-                      return Divider(color: subtitleColor, height: 1.0);
-                    },
-                    itemCount: 10,
-                  ),
-                ),
-              ),
-            ),
+        child: XafeAnimatedIndexedStack(
+          index: 0,
+          children: <Widget>[
+            CategoriesScreen(),
+            HomeScreen(),
           ],
         ),
       ),
