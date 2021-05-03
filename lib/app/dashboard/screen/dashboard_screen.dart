dc659a664552f409e6e3f86c370d29a82b393ae7
Israel Akpan <isrealakpan296@gmail.com>
2021-05-03 04:22:21 +0100

    built the listview

---
+++ lib/app/dashboard/screen/dashboard_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import '../../../cores/constants/colors.dart';
+import '../../../cores/constants/styles.dart';
+import '../widgets/xafe_bottom_navigation_bar.dart';
+
+/// [Dashboard] screen or app main home page
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+            ),
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: cardColor,
+                  ),
+                  child: ListView.separated(
+                    itemBuilder: (context, i) {
+                      return Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                '📄 Bill Payments',
+                                style: textStyle.copyWith(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: titleColor,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              '\$500.00',
+                              style: textStyle.copyWith(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500,
+                                color: titleColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, i) {
+                      return Divider(color: subtitleColor, height: 1.0);
+                    },
+                    itemCount: 10,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: XafeBottomNavigationBar(),
+    );
+  }
+}
