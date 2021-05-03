b871c56d7247f90942fe245a81e4c7fba717fdd2
Israel Akpan <isrealakpan296@gmail.com>
2021-05-03 04:01:45 +0100

    implemented the navigationBar

---
+++ lib/app/dashboard/widgets/xafe_bottom_navigation_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/widgets.dart';
+import '../../../cores/constants/colors.dart';
+import '../../../cores/constants/styles.dart';
+
+/// [XafeBottomNavigationItem]
+class XafeBottomNavigationItem extends StatelessWidget {
+  /// icon
+  final Widget icon;
+
+  /// item label
+  final String label;
+
+  /// is item selected
+  final bool selected;
+
+  /// to show on selected
+  final Widget activeIcon;
+
+  /// [XafeBottomNavigationItem] constructor
+  XafeBottomNavigationItem({
+    Key key,
+    this.icon,
+    this.label,
+    this.selected = false,
+    this.activeIcon,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(child: selected ? activeIcon : icon),
+        SizedBox(height: 5.0),
+        Text(
+          label,
+          style: textStyle.copyWith(
+            fontSize: 12.0,
+            fontWeight: FontWeight.w400,
+            color: selected ? titleColor : subtitleColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
