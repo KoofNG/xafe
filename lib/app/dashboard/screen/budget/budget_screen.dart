b9ce00a96c9660737d8b878438b710bc09ee247c
Israel Akpan <isrealakpan296@gmail.com>
2021-05-03 05:10:37 +0100

    [WIP]

---
+++ lib/app/dashboard/screen/budget/budget_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+/// [Budget]
+class BudgetScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
