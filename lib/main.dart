78b41198309fd3327bae57d0e7a4f8eee2d5f58d
Israel Akpan <isrealakpan296@gmail.com>
2021-05-03 20:08:10 +0100

    wrapped with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 import 'cores/constants/colors.dart';
 import 'cores/constants/routes.dart';
 import 'cores/routes/appRoutes.dart';
+import 'cores/setups/providers.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
   // Enforce portrait mode
@@ -21,14 +25,17 @@ void main() {
 class XafeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: cardColor,
-        fontFamily: 'Euclid',
+    return MultiProvider(
+      providers: providers,
+      child: MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: cardColor,
+          fontFamily: 'Euclid',
+        ),
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: generateRoute,
+        initialRoute: homeScreen,
       ),
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: generateRoute,
-      initialRoute: homeScreen,
     );
   }
 }
